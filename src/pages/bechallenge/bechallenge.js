5c301adaaf0e23cef7c065b44f7fd73f1e61d1b5
fahadsaleem1 <igz.iwd.25@gmail.com>
2020-12-18 14:41:14 +0500

    add

--- src/pages/bechallenge/bechallenge.js
+++ src/pages/bechallenge/bechallenge.js
@@ -93,10 +93,28 @@ const BeChallenege = () => (
                         </div>
                         
                      </Row>
-                  
-    
-                    
-                    
+                    </Container>
+                    <Container>
+                        <Row>
+                        <Col sm={3} xs={3}  lg={3} md={3}></Col>
+                        <Col sm={6} xs={6}  lg={6} md={6}>
+                            <h1>Title</h1>
+                            <p>Lorem Ipsum is simply dummy text of the printing 
+                                and typesetting industry. Lorem Ipsum has been the
+                                industry's standard dummy text ever since the 1500s,
+                                when an unknown printer took a galley of type and scrambled
+                                it to make a type specimen book. It has survived not only five centuries, but 
+                                also the leap into electronic typesetting, remaining essentially unchanged.
+                                It was popularised in the 1960s with the release of Letraset sheets containing 
+                                Lorem Ipsum passages, and more recently with desktop publishing
+                                software like Aldus PageMaker including versions of Lorem Ipsum</p>
+                                <Image  src={data.allMarkdownRemark.edges[0].node.frontmatter.image1} className="center"  style={{minHeight:"500px"}} fluid/>
+
+                        </Col>
+                        <Col sm={3} xs={3}  lg={3} md={3}></Col>
+
+                        </Row>
+
                     </Container>
             </div>  
                    
